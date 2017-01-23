be0c5be625df8b0e9419a4bfc6f3f4169fc563f8
chuckis <ishchuk@linuxmint.linuxmint-domain>
2017-01-23 23:40:05 +0200

    add 3 components

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
+var News = React.createClass({
+    render: function(){
+        return (
+            <div className="news">
+                К сожалению новостей нет
+            </div>
+                );
+    }
+});
+
+var Comments = React.createClass({
+    render: function(){
+        return (
+            <div className="comments">
+                Нет новостей - нечего комментировать...
+            </div>
+                );
+    }
+});
+
+var App = React.createClass({
+    render: function(){
+        return (
+            <div className="app"> Привет я компонент App я могу отображать новости
+            <News />
+            <Comments/>
+            </div>
+                );
+    }
+});
+
 ReactDOM.render(
-    <h1>Привет мир</h1>,
+   <App />,
     document.getElementById('root')
 );
