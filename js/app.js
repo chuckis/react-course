0ac0b9b491ab33f32c470c28839a059f5d5e96e4
chuckis <ishchuk@linuxmint.linuxmint-domain>
2017-01-25 23:57:24 +0200

    add newsTemplate

--- js/app.js
+++ js/app.js
@@ -1,10 +1,37 @@
+var my_news = [
+    {
+        author: 'Саша Пушкин',
+        text: ' Я помню...'
+    },
+    {
+        author: 'Колесо обозрения',
+        text: 'Меня снесли в пункт стеклотары'
+    },
+    {
+        author: 'Бродяга',
+        text: 'Бананов не надо а'
+    }
+];
+
 var News = React.createClass({
     render: function(){
-        return (
-            <div className="news">
-                К сожалению новостей нет
-            </div>
+        var data = this.props.data;
+        var newsTemplate = data.map(function(item, index){
+
+            return (
+                <div key = {index}>
+                    <p className="news__author">{item.author}: </p>
+                    <p className="news__text">{item.text}</p>
+                    </div>
                 );
+        });
+
+
+        return (
+                <div className="news">
+                {newsTemplate}
+            </div>
+        );
     }
 });
 
@@ -22,7 +49,7 @@ var App = React.createClass({
     render: function(){
         return (
             <div className="app"> Привет я компонент App я могу отображать новости
-            <News />
+            <News data={my_news} />
             <Comments/>
             </div>
                 );
