433a1e32dd215a3746707d2eff3b142787854191
chuckis <ishchuk@linuxmint.linuxmint-domain>
2017-01-27 00:01:36 +0200

    no news is good news

--- js/app.js
+++ js/app.js
@@ -1,35 +1,47 @@
-var my_news = [
-    {
-        author: 'Саша Пушкин',
-        text: ' Я помню...'
-    },
-    {
-        author: 'Колесо обозрения',
-        text: 'Меня снесли в пункт стеклотары'
-    },
-    {
-        author: 'Бродяга',
-        text: 'Бананов не надо а'
-    }
-];
+// var my_news = [
+//     {
+//         author: 'Саша Пушкин',
+//         text: ' Я помню...'
+//     },
+//     {
+//         author: 'Колесо обозрения',
+//         text: 'Меня снесли в пункт стеклотары'
+//     },
+//     {
+//         author: 'Бродяга',
+//         text: 'Бананов не надо а'
+//     }
+// ];
+
+var my_news = [];
 
 var News = React.createClass({
     render: function(){
         var data = this.props.data;
-        var newsTemplate = data.map(function(item, index){
+        var newsTemplate;
 
-            return (
-                <div key = {index}>
-                    <p className="news__author">{item.author}: </p>
-                    <p className="news__text">{item.text}</p>
-                    </div>
+        if( data.length >0 ){
+            newsTemplate = data.map(function(item, index){
+
+                return (
+                        <div key = {index}>
+                        <p className="news__author">{item.author}: </p>
+                        <p className="news__text">{item.text}</p>
+                        </div>
                 );
-        });
+
+            });
+        } else {
+            newsTemplate = <p>Новостеей нет </p>;
+
+
+        }
 
 
         return (
                 <div className="news">
                 {newsTemplate}
+            <strong>Всего новостей: {data.length}</strong>
             </div>
         );
     }
